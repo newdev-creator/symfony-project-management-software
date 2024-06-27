d6808beafbc13447d4e43187618ed339e64b5000
Julien Freisa <newdevcreator@gmail.com>
2024-06-27 16:59:00 +0200

    FEAT: dashboard controller

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -17,6 +18,11 @@ class SecurityController extends AbstractController
 
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
+
+        // if user is already connected, redirect to dashboard
+        if ($this->getUser()) {
+            return new RedirectResponse($this->generateUrl('dashboard_index'));
+        }
 
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
